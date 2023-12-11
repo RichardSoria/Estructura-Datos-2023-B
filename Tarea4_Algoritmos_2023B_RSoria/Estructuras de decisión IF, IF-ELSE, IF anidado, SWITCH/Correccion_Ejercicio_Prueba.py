ae6ab3c4a26cd0ced693cd14774f8519d130b386
RichardSoria <richard.soria@epn.edu.ec>
2023-12-10 21:59:25 -0500

    Tarea4_Algoritmos_2023B_RSoria

---
+++ Tarea4_Algoritmos_2023B_RSoria/Estructuras de decisión IF, IF-ELSE, IF anidado, SWITCH/Correccion_Ejercicio_Prueba.py
@@ -0,0 +1,106 @@
+
+# Corrección Ejercicio Prueba If-Else #
+
+# Richard Mauricio Soria Asanza #
+
+print("********** Bienvenido a las hamburguesas CARBONERO **********")
+print("Contamos con diferentes hamburguesas y métodos de pago")
+print("Ofrecemos los siguientes tipos de hamburguesas:")
+print("1) Sencilla $1.50")
+print("2) Doble $2.50")
+print("3) Triple $3.25")
+tipo_hamburguesa = int(input("Ingrese el tipo de hamburguesa que desea, digitando el número que le corresponde:\n"))
+
+if tipo_hamburguesa == 1:
+    cantidad_hamburguesa = int(input("Ingrese la cantidad de hamburguesas que desea comprar:\n"))
+
+    pago_efectivo_simple = 1.50*cantidad_hamburguesa
+    pago_credito_simple = 1.50*cantidad_hamburguesa+pago_efectivo_simple*0.05
+
+    print("Usted ha selecionado Hamburguesas Simples con una cantidad de", cantidad_hamburguesa, ",selecione el método de pago:")
+    print("1) Efectivo")
+    print("2) Tarjeta de crédito (5% recargo)")
+    tipo_pago = int(input("Ingrese el método de pago, digitando el número que le corresponde:\n"))
+    if tipo_pago == 1:
+        print("Usted ha selecionado como método de pago efectivo:")
+        print("Su total a cancelar es de:\n$",round(pago_efectivo_simple,2), "por una cantidad de", cantidad_hamburguesa, "Hamburguesas Simples")
+        print("Porfavor digite los datos para su factura")
+        nombre = str(input("Ingrese su nombre:\n"))
+        cedula = str(input("Ingrese su cédula:\n"))
+        correo = str(input("Ingrese su correro electrónico:\n"))
+        print("Muchas gracias por su compra", nombre, "la factura será enviada a su correo", correo,".")
+        print("Vuelva pronto, lo estaremos esperando.")
+    elif tipo_pago == 2:
+        print("Usted ha selecionado como método de pago trajeta de crédito:")
+        print("Su total a cancelar es de:\n$",round(pago_credito_simple,2), "por una cantidad de", cantidad_hamburguesa, "Hamburguesas Simples con un recargo de 5%")
+        print("Porfavor digite los datos para su factura")
+        nombre = str(input("Ingrese su nombre:\n"))
+        cedula = str(input("Ingrese su cédula:\n"))
+        correo = str(input("Ingrese su correro electrónico:\n"))
+        print("Muchas gracias por su compra", nombre, "la factura será enviada a su correo", correo,".")
+        print("Vuelva pronto, lo estaremos esperando.")
+    else:
+        print("No es un método de pago válido.")
+elif tipo_hamburguesa == 2:
+    cantidad_hamburguesa = int(input("Ingrese la cantidad de hamburguesas que desea comprar:\n"))
+
+    pago_efectivo_doble = 2.50*cantidad_hamburguesa
+    pago_credito_doble = 2.50*cantidad_hamburguesa+pago_efectivo_doble*0.05
+    
+    print("Usted ha selecionado Hamburguesas Dobles con una cantidad de", cantidad_hamburguesa, ",selecione el método de pago:")
+    print("1) Efectivo")
+    print("2) Tarjeta de crédito (5% recargo)")
+    tipo_pago = int(input("Ingrese el método de pago, digitando el número que le corresponde:\n"))
+    if tipo_pago == 1:
+        print("Usted ha selecionado como método de pago efectivo:")
+        print("Su total a cancelar es de:\n$",round(pago_efectivo_doble,2), "por una cantidad de", cantidad_hamburguesa, "Hamburguesas Dobles")
+        print("Porfavor digite los datos para su factura")
+        nombre = str(input("Ingrese su nombre:\n"))
+        cedula = str(input("Ingrese su cédula:\n"))
+        correo = str(input("Ingrese su correro electrónico:\n"))
+        print("Muchas gracias por su compra", nombre, "la factura será enviada a su correo", correo,".")
+        print("Vuelva pronto, lo estaremos esperando.")
+    elif tipo_pago == 2:
+        print("Usted ha selecionado como método de pago trajeta de crédito:")
+        print("Su total a cancelar es de:\n$",round(pago_credito_doble,2), "por una cantidad de", cantidad_hamburguesa, "Hamburguesas Dobles con un recargo de 5%")
+        print("Porfavor digite los datos para su factura")
+        nombre = str(input("Ingrese su nombre:\n"))
+        cedula = str(input("Ingrese su cédula:\n"))
+        correo = str(input("Ingrese su correro electrónico:\n"))
+        print("Muchas gracias por su compra", nombre, "la factura será enviada a su correo", correo,".")
+        print("Vuelva pronto, lo estaremos esperando.")
+    else:
+        print("No es un método de pago válido.")
+elif tipo_hamburguesa == 3:
+    cantidad_hamburguesa = int(input("Ingrese la cantidad de hamburguesas que desea comprar:\n"))
+
+    pago_efectivo_triple = 3.25*cantidad_hamburguesa
+
+    pago_credito_triple = 3.25*cantidad_hamburguesa+pago_efectivo_triple*0.05
+
+    print("Usted ha selecionado Hamburguesas Simples con una cantidad de", cantidad_hamburguesa, ",selecione el método de pago:")
+    print("1) Efectivo")
+    print("2) Tarjeta de crédito (5% recargo)")
+    tipo_pago = int(input("Ingrese el método de pago, digitando el número que le corresponde:\n"))
+    if tipo_pago == 1:
+        print("Usted ha selecionado como método de pago efectivo:")
+        print("Su total a cancelar es de:\n$",round(pago_efectivo_triple,2), "por una cantidad de", cantidad_hamburguesa, "Hamburguesas Triples")
+        print("Porfavor digite los datos para su factura")
+        nombre = str(input("Ingrese su nombre:\n"))
+        cedula = str(input("Ingrese su cédula:\n"))
+        correo = str(input("Ingrese su correro electrónico:\n"))
+        print("Muchas gracias por su compra", nombre, "la factura será enviada a su correo", correo,".")
+        print("Vuelva pronto, lo estaremos esperando.")
+    elif tipo_pago == 2:
+        print("Usted ha selecionado como método de pago trajeta de crédito:")
+        print("Su total a cancelar es de:\n$",round(pago_credito_triple,2), "por una cantidad de", cantidad_hamburguesa, "Hamburguesas Triples con un recargo de 5%")
+        print("Porfavor digite los datos para su factura")
+        nombre = str(input("Ingrese su nombre:\n"))
+        cedula = str(input("Ingrese su cédula:\n"))
+        correo = str(input("Ingrese su correro electrónico:\n"))
+        print("Muchas gracias por su compra", nombre, "la factura será enviada a su correo", correo,".")
+        print("Vuelva pronto, lo estaremos esperando.")
+    else:
+        print("No es un método de pago válido.")
+else:
+    print("Ese tipo de hamburguesa no se encuentra dentro del menú.")
---
+++ Tarea4_Algoritmos_2023B_RSoria/Estructuras de decisión IF, IF-ELSE, IF anidado, SWITCH/Ejercicio_Calculadora.py
@@ -0,0 +1,43 @@
+
+# Ejercicio Calculadora #
+
+# Richard Mauricio Soria Asanza #
+
+print("Calculadora de operaciones básicas")
+num_1 = float(input("Ingrese el primer número:\n"))
+num_2 = float(input("Ingrese el segundo número:\n"))
+
+suma = num_1 + num_2
+resta = num_1 - num_2
+producto = num_1 * num_2
+potencia = num_1 ** num_2
+
+print("Opción 1: Suma")
+print("Opción 2: Resta")
+print("Opción 3: Multiplicación")
+print("Opción 4: División (Recuerde no se puede dividir para 0)")
+print("Opción 5: Potencia")
+print("Opción 6: Módulo (Recuerde no se puede obtener el módulo para 0)")
+opcion = int(input("Ingrese la operación que desea realizar:\n"))
+
+if opcion == 4 and num_2 ==0:
+    print("No se puede dividir para cero")
+elif opcion == 6 and num_2 ==0:
+    print("No se puede obtener el módulo para cero")
+else:
+    match opcion:
+        case 1:
+            print("La suma de los números",num_1,",",num_2,"es:\n",suma)
+        case 2:
+            print("La resta de los números",num_1,",",num_2,"es:\n",resta)
+        case 3:
+            print("La multiplicación de los números",num_1,",",num_2,"es:\n",producto)
+        case 4:
+            division = num_1 / num_2
+            print("La división de los números",num_1,",",num_2,"es:\n",division)
+        case 5:
+            print("El producto de los números",num_1,",",num_2,"es:\n",potencia)
+        case 6:
+            modulo = num_1 % num_2
+            print("La suma de los números",num_1,",",num_2,"es:\n",modulo)
+        case other: print("No es una operación válida")
